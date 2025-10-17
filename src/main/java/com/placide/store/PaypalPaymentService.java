cf52d9e459f9e180c02de4f95ffa376ee5ff2193
Placide IRANDORA <placideirandora@gmail.com>
2025-10-17 19:27:38 +0200

    refactor: use dependency injection to decouple the order service from the payment service
    Now, we have extended our order service without having to modify the payment logic inside the method itself. We can add more payment services freely. We have applied the Open Close Principle in OOP

--- src/main/java/com/placide/store/PaypalPaymentService.java
+++ src/main/java/com/placide/store/PaypalPaymentService.java
@@ -1,9 +1,10 @@
 package com.placide.store;
 
-public class StripePaymentService implements PaymentService {
+public class PaypalPaymentService implements PaymentService {
+
     @Override
     public void processPayment(double amount) {
-        System.out.println("Stripe Payment");
+        System.out.println("Paypal Payment");
         System.out.println("Amount : " + amount + "\n");
     }
 }
